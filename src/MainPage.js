d334f586a6798acfbb5b2765ee7c40a6b7877f54
ridk <ridk@qq.com>
2019-07-15 18:01:48 +0800

    第一次提交

--- src/MainPage.js
+++ src/MainPage.js
@@ -13,6 +13,15 @@ class  MainPage extends React.Component{
     componentWillUnmount() {
     // TODO 消失的时候执行
     }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
 export  default  MainPage;
